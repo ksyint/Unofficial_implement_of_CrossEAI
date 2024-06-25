d8cc71a878d4b04220e4fe666278a6111e60c6ae
ksyint <82583462+ksyint@users.noreply.github.com>
2024-06-25 23:35:40 +0900

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -6,8 +6,7 @@ class ResNet50(nn.Module):
     def __init__(self):
         super(ResNet50, self).__init__()
         self.backbone = models.resnet50(pretrained=True)
-        self.backbone.fc = nn.Identity()  # Remove the classification layer
-        self.fc = nn.Linear(self.backbone.fc.in_features, 2)  # Add new classification layer
+        self.fc = nn.Linear(self.backbone.fc.in_features, 2)  
 
     def forward(self, x):
         features = self.backbone(x)
